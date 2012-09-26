781254f4822363792f0a0c20869c87c0279d4e36
Pavel Tikhonov <pash3r@gmail.com>
2012-09-26 19:32:46 +0400

    добавил uinavigationcontroller

---
+++ instagramAppTest/detailViewController.m
@@ -0,0 +1,44 @@
+//
+//  detailViewController.m
+//  instagramAppTest
+//
+//  Created by Roman Klimov on 26.09.12.
+//  Copyright (c) 2012 info@vvww.ru. All rights reserved.
+//
+
+#import "detailViewController.h"
+
+@interface detailViewController ()
+
+@end
+
+@implementation detailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
