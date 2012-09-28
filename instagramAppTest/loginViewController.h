a6aa868478520979c22e3fb0eafe480e084dad28
Pavel Tikhonov <pash3r@gmail.com>
2012-09-28 13:27:47 +0400

    добавил loginViewController, пока что борюсь с получением токена.

---
+++ instagramAppTest/loginViewController.h
@@ -0,0 +1,13 @@
+//
+//  loginViewController.h
+//  instagramAppTest
+//
+//  Created by Roman Klimov on 27.09.12.
+//  Copyright (c) 2012 info@vvww.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginViewController : UIViewController
+
+@end
---
+++ instagramAppTest/loginViewController.m
@@ -0,0 +1,44 @@
+//
+//  loginViewController.m
+//  instagramAppTest
+//
+//  Created by Roman Klimov on 27.09.12.
+//  Copyright (c) 2012 info@vvww.ru. All rights reserved.
+//
+
+#import "loginViewController.h"
+
+@interface loginViewController ()
+
+@end
+
+@implementation loginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
